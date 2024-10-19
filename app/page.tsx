6ca39f830a6ec09fc7391945e5f73e2a7a1fbb2f
Ehindero Israel <iehindero419@stu.ui.edu.ng>
2024-10-19 18:29:48 +0100

    Fix initial dep error

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import Image from "next/image"
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-4 row-start-2 items-center sm:items-start">
         <h4 className="text-8xl font-bold font-serif">Dero</h4>
-        <span>I'll be back here in a jiffy.</span>
+        <span>{`I'll be back here in a jiffy.`}</span>
       </main>
     </div>
   )
